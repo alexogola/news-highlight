fb47a751dcf80d2ace8a8f0dd71675923d027ffc
Alex Ogola <ogollaokumu@gmail.com>
2019-02-02 10:57:08 +0300

    add code to get data from API

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import articles
+from .models import sources
 
 # Getting api key
 api_key = app.config['SOURCES_API_KEY']
+
+
+Articles = articles.Articles
+Sources = sources.Sources
